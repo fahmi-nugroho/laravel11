a21e401c5985129e832f49a4b95d8be61ce87096
fahmi-nugroho <fahminugroho23@gmail.com>
2024-09-16 15:20:27 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,24 @@
 // NOTE harus menggunakan namspace agar bisa terbaca oleh Laravel
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+// NOTE Secara default model Post akan berpasangan dengan tabel posts
+class Post extends Model
 {
-  public static function all()
-  {
-    return [
-      [
-        'id' => '1',
-        'title' => 'Judul Artikel 1',
-        'slug' => 'judul-artikel-1',
-        'author' => 'Fahmi Nugroho',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eum quasi dicta quod! Sed, cumque dicta sit neque laborum itaque alias voluptate. Excepturi dolore sunt enim quod fuga. Eaque, corrupti.'
-      ],
-      [
-        'id' => '2',
-        'title' => 'Judul Artikel 2',
-        'slug' => 'judul-artikel-2',
-        'author' => 'Fahmi Nugroho',
-        'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore quis ab tempore commodi ad provident assumenda soluta iure, eligendi, ratione fugiat veritatis blanditiis voluptatibus ipsa corporis perferendis asperiores nesciunt! Earum minima minus aperiam illo error fugit. Rem, doloremque itaque! Eum dolorem tempore dolores reprehenderit ipsum quasi, reiciendis error quam amet aperiam suscipit architecto, harum aliquid aspernatur, perspiciatis sapiente quis nam debitis adipisci asperiores. Rerum quidem facilis molestiae repellendus harum, obcaecati illo soluta, sit architecto iure tempore placeat eaque aperiam alias eos dolorum ipsum doloremque! Blanditiis asperiores eius reprehenderit similique modi corporis, distinctio a ab, explicabo omnis magnam dolores laborum odit minus. Molestias perspiciatis totam nihil libero molestiae ab optio quod ad, perferendis aut, id deserunt tempora corrupti! Molestias voluptate expedita eius a, officia in et, numquam, iste necessitatibus aliquid alias tempora fuga quaerat esse ex accusamus repellendus aut iure saepe suscipit vero aspernatur veniam? Tenetur placeat ipsum sunt? Esse, eaque!'
-      ]
-    ];
-  }
+    use HasFactory;
+    // NOTE Ini dibutuhkan jika nama tabel tidak sesuai penamaan default laravel
+    // protected $table = 'blog_post';
 
-  public static function find($slug): array
-  {
-    // NOTE contoh arrow function di php
-    $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
+    // NOTE Ini dibutuhkan jika primary key tidak seesuai dengan default laravel (id)
+    // protected $primaryKey = 'post_id';
 
-    if (!$post) {
-      abort(404);
-    }
+    // NOTE Method all sudah ada secara default pada Model
 
-    return $post;
-  }
+    // NOTE Untuk menentukan field apa saja yang boleh diisi manual
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    // NOTE Untuk menentukan filed apa saja yang tidak boleh diisi manual
+    // protected $guarded = [];
 }
